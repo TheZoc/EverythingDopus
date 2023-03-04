cd6b3c4c5b1922a844cbd9b1dae88c60daa656cc
Felipe Guedes <TheZoc@users.noreply.github.com>
2023-03-04 02:11:08 +0000

    - Cleaned up Make-Release.ps1 script - Added Directory Opus toolbar (dcf) file to Make-Release.ps1 script

--- Make-Release.ps1
+++ Make-Release.ps1
@@ -1,3 +1,8 @@
+$script:SevenZip = "${env:ProgramFiles}\7-Zip\7z.exe"
+$script:VSDevShell = "${env:ProgramFiles}\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1"
+
+& $script:VSDevShell -SkipAutomaticLocation
+
 Remove-Item .\Out\ -Recurse -Verbose -ErrorAction SilentlyContinue
 Remove-Item .\x64\ -Recurse -Verbose -ErrorAction SilentlyContinue
 Remove-Item .\Release\ -Recurse -Verbose -ErrorAction SilentlyContinue
@@ -15,5 +20,11 @@ Copy-Item -Path .\Release\ed.exe -Destination ".\Out\x86\"
 Copy-Item -Path .\libs\Everything-SDK\dll\Everything64.dll -Destination ".\Out\x64\"
 Copy-Item -Path .\libs\Everything-SDK\dll\Everything32.dll -Destination ".\Out\x86\"
 
-&'C:\Program Files\7-Zip\7z.exe' a -tzip .\Out\x64\EverythingDopus.osp ".\DirectoryOpus\*"
-&'C:\Program Files\7-Zip\7z.exe' a -tzip .\Out\x86\EverythingDopus.osp ".\DirectoryOpus\*"
+& $script:SevenZip a -tzip .\Out\x64\EverythingDopus.osp ".\DirectoryOpus\*"
+& $script:SevenZip a -tzip .\Out\x86\EverythingDopus.osp ".\DirectoryOpus\*"
+
+Copy-Item -Path .\resources\EverythingDopus.dcf -Destination ".\Out\x64\"
+Copy-Item -Path .\resources\EverythingDopus.dcf -Destination ".\Out\x86\"
+
+& $script:SevenZip a -tzip .\Out\EverythingDopus-x64.zip ".\Out\x64\*"
+& $script:SevenZip a -tzip .\Out\EverythingDopus-x86.zip ".\Out\x86\*"
